0e0a9fd4d19f2855099381a7f6d40f7f97e09cba
Raul Ruiz <raul.ruizlvalle@alu.uclm.es>
2021-01-23 11:49:08 +0100

    freeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <PubSubClient.h>
+#include "freertos/FreeRTOS.h"
 
 const char *ssid = "g5Net";
 const char *password = "g5IotNet";
@@ -11,13 +12,150 @@ const char *password = "g5IotNet";
 #define DHTPIN D4
 #define DHTTYPE DHT11
 #define TOPIC "g5/sensor"
-#define BROKER_IP "192.168.43.14"
+#define BROKER_IP "192.168.1.144"
 #define BROKER_PORT 2883
 
 DHT dht(DHTPIN, DHTTYPE);
+#define LIGHTSENSORPIN A1
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+QueueHandle_t xMutex;
+
+float temperatura;
+float humedad;
+float humedadSuelo;
+float fotoreceptor;
+
+void IRAM_ATTR on_handleInterrupt()
+{
+}
+
+void IRAM_ATTR off_handleInterrupt()
+{
+}
+
+/*
+  Tarea para la lectura de la temperatura global y escogida y arrancado de termostato
+*/
+void tareaTemperatura(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      temperatura = dht.readTemperature();
+      if (isnan(temperatura))
+      {
+        Serial.println(F("Error de lectura del sensor DHT!"));
+        return;
+      }
+
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para la lectura de la luz ambiente
+*/
+void tareaFotoreceptor(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      fotoreceptor = analogRead(LIGHTSENSORPIN);
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para la lectura de la humedad ambiente
+*/
+void tareaHumedad(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      humedad = dht.readHumidity();
+      if (isnan(humedad))
+      {
+        Serial.println(F("Error de lectura del sensor DHT!"));
+        return;
+      }
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para la lectura de la humedad del suelo
+*/
+void tareaHumedadSuelo(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      humedadSuelo = 15.0;
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para activar el servo
+*/
+void tareaServo(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      if (temperatura > 30 || humedad < 30  ){
+        printf("Accionar servo");
+      }
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1000);
+    }
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para activar el servo
+*/
+void tareaEnvio(void *param)
+{
+  for (;;)
+  {
+    if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE)
+    {
+      TickType_t xLastWakeTime = xTaskGetTickCount();
+      String jsonData = "{\"temperatura\":" + String(temperatura) + ",\"luz\":" + String(fotoreceptor) + ",\"humedad\":" + String(humedad) + ",\"humedadSuelo\":" + String(humedadSuelo) + "}";
+      client.publish(TOPIC, jsonData.c_str());
+      xSemaphoreGive(xMutex);
+      vTaskDelayUntil(&xLastWakeTime, 1500);
+    }
+  }
+  vTaskDelete(NULL);
+}
 
 void wifiConnect()
 {
@@ -64,18 +202,19 @@ void setup()
   wifiConnect();
   mqttConnect();
 
-  //client.subscribe(TOPIC);
-  //client.setCallback(mqttCallback);
+  xMutex = xSemaphoreCreateMutex();
+  if (xMutex != NULL)
+  {
+    xTaskCreatePinnedToCore(tareaTemperatura, "Tarea lectura temperatura", 1500, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(tareaFotoreceptor, "Tarea lectura luz", 1500, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(tareaHumedad, "Tarea lectura humedad", 1500, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(tareaHumedadSuelo, "Tarea lectura humedad del suelo", 1500, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(tareaServo, "Tarea accionador servo", 1500, NULL, 1, NULL, 0);
+    xTaskCreatePinnedToCore(tareaEnvio, "Tarea para enviar", 1500, NULL, 1, NULL, 0);
+  }
+
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  // put your main code here, to run repeatedly:
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
-  String jsonData = "{\"temperature\":" + String(t) + ",\"humidity\":" + String(h) + "}";
-
-  client.publish(TOPIC, jsonData.c_str());
-  delay(3000);
 }
