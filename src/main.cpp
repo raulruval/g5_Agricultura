cd6c58ead4724407a9f872c15a6788f54c9b11a7
RubenPerez7 <ruben.perez7@alu.uclm.es>
2021-01-29 20:52:28 +0100

    Ahorro energético, temporizador y refactorización de código

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,41 @@
 #include <pb.h>
 #include <pb_encode.h>
 #include "map"
+#include <esp_wifi.h>
+#include "driver/timer.h"
+#include <soc/sens_reg.h>
 
-const char *ssid = "g5Net3";
+const char *ssid = "g5Net2";
 const char *password = "g5IotNet";
 
 #define DHTPIN D4
 #define DHTTYPE DHT11
 #define TOPIC "g5/sensor"
 #define TOPICNODERED "g5/nodered"
-#define BROKER_IP "192.168.140.178"
+#define BROKER_IP "192.168.18.47"
 #define BROKER_PORT 2883
 #define LIGHTSENSORPIN A1
 #define SENSORSUELO A3
 #define BTN_MOTOR D2
+#define BTN_DESPERTAR D3
 
 DHT dht(DHTPIN, DHTTYPE);
 std::map<String, float> Map;
+uint64_t reg_a;
+uint64_t reg_b;
+uint64_t reg_c;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 xQueueHandle queue;
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 
 float temperatura = 0.0;
 float humedad = 0.0;
 float humedadSuelo = 0.0;
 float fotoreceptor = 0.0;
+volatile int interruptCounter;
 
 void IRAM_ATTR on_handleInterrupt()
 {
@@ -45,129 +55,8 @@ void IRAM_ATTR off_handleInterrupt()
 {
 }
 
-/*
-  Tarea para activar el servo
-*/
-void servo(bool activate)
-{
-  if (activate)
-  {
-    Serial.println("Accionar servo");
-    /* Movimiento del servo cada 2 segundos en diferentes ángulos
-        mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 2.5);
-        //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 500);  //0.5ms - 0
-        delay(2000);
-        mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 7.5);
-        //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1500); //1.5ms - 90
-        delay(2000);
-        mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 12.5);
-        //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 2500); //2.5ms - 180
-        delay(2000);*/
-  }
-  else
-  {
-    Serial.println("Parar servo");
-  }
-}
-
-void IRAM_ATTR motor_interrupcion()
-{
-  if (digitalRead(BTN_MOTOR) == 0)
-  {
-    servo(true);
-  }
-}
-
-/*
-  Tarea para la lectura de la temperatura global y escogida y arrancado de termostato
-*/
-void tareaTemperatura(void *param)
-{
-  for (;;)
-  {
-    Map["t"] = dht.readTemperature();
-    xQueueSend(queue, (void *)&Map, (TickType_t)0);
-    vTaskDelay(2000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
-}
-
-/*
-  Tarea para la lectura de la luz ambiente
-*/
-void tareaFotoreceptor(void *param)
-{
-  for (;;)
-  {
-    Map["f"] = analogRead(LIGHTSENSORPIN);
-    xQueueSend(queue, (void *)&Map, (TickType_t)0);
-    vTaskDelay(3000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
-}
-/*
-  Tarea para la lectura de la humedad ambiente
-*/
-void tareaHumedad(void *param)
-{
-  for (;;)
-  {
-    Map["h"] = dht.readHumidity();
-    xQueueSend(queue, (void *)&Map, (TickType_t)0);
-    vTaskDelay(6000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
-}
-
-/*
-  Tarea para la lectura de la humedad del suelo
-*/
-void tareaHumedadSuelo(void *param)
-{
-  for (;;)
-  {
-    Map["hs"] = 15.0;
-    //El sensor registra valores normales de humedad, no es necesaria conversión. Valor registrado = 33~34
-    //humedadSuelo = SENSORSUELO; Si se introduce con analogRead, obtenemos el valor en millares y decimales. Se puede utilizar si queremos se más precisos
-    xQueueSend(queue, (void *)&Map, (TickType_t)0);
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
-}
-
-/*
-  Tarea para Enviar la información a la arquitectura
-*/
-void tareaEnvio(void *param)
-{
-  for (;;)
-  {
-    xQueueReceive(queue, &Map, (TickType_t)(1000 / portTICK_PERIOD_MS));
-    String jsonData = "{\"temperatura\":" + String(Map["t"]) + ",\"fotoreceptor\":" + String(Map["f"]) + ",\"humedad\":" + String(Map["h"]) + ",\"humedadSuelo\":" + String(Map["hs"]) + "}";
-    Serial.printf("\nEnviando información...");
-    
-    client.publish(TOPIC, jsonData.c_str());
-
-    // client.publish(TOPICNORERED, jsonData.c_str());
-    nodeRed message = nodeRed_init_zero;
-    message.temperatura = Map["t"];
-    message.humedad = Map["h"];
-    message.humedadSuelo = Map["hs"];
-    message.fotoreceptor = Map["f"];
-    message.has_temperatura = true;
-    message.has_humedad = true;
-    message.has_humedadSuelo = true;
-    message.has_fotoreceptor = true;
-
-    uint8_t buffer[200];
-    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-    bool status = pb_encode(&stream, nodeRed_fields, &message);
-    if (!status)
-      Serial.println("Error encode");
-    client.publish(TOPICNODERED, buffer, stream.bytes_written);
-    vTaskDelay(7000 / portTICK_PERIOD_MS);
-  }
-  vTaskDelete(NULL);
+void wifiDisconnect() {
+  WiFi.disconnect();
 }
 
 void wifiConnect()
@@ -202,19 +91,169 @@ void mqttConnect()
   }
 }
 
+/*
+  Tarea para activar el servo
+*/
+void servo()
+{
+  
+  Serial.println("Accionar servo");
+  //Movimiento del servo cada 2 segundos en diferentes ángulos
+  mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 2.5);
+  //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 500);  //0.5ms - 0
+  delay(2000);
+  mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 7.5);
+  //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 1500); //1.5ms - 90
+  delay(2000);
+  mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 12.5);
+  //mcpwm_set_duty_in_us(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, 2500); //2.5ms - 180
+  delay(2000);
+}
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+void IRAM_ATTR motor_interrupcion()
+{
+  if (digitalRead(BTN_MOTOR) == 0)
+  {
+    servo();
+  }
+}
+
+/*
+  Tarea para la lectura de la temperatura global y escogida y arrancado de termostato
+*/
+void tareaTemperatura(void *param)
+{
+  for (;;)
+  {
+    if (interruptCounter > 0) {
+      portENTER_CRITICAL(&timerMux);
+      interruptCounter--;
+      portEXIT_CRITICAL(&timerMux);
+
+      Serial.print("Ha ocurrido una interrupcion");
+      esp_deep_sleep_start();
+
+    }
+    Map["t"] = dht.readTemperature();
+    xQueueSend(queue, (void *)&Map, (TickType_t)0);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para la lectura de la luz ambiente
+*/
+void tareaFotoreceptor(void *param)
+{
+  for (;;)
+  {
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Desconectado");
+    }
+    Map["f"] = analogRead(LIGHTSENSORPIN);
+    Serial.print(Map["f"]);
+    xQueueSend(queue, (void *)&Map, (TickType_t)0);
+    vTaskDelay(3000 / portTICK_PERIOD_MS);
+  }
+  vTaskDelete(NULL);
+}
+/*
+  Tarea para la lectura de la humedad ambiente
+*/
+void tareaHumedad(void *param)
+{
+  for (;;)
+  {
+    Map["h"] = dht.readHumidity();
+    xQueueSend(queue, (void *)&Map, (TickType_t)0);
+    vTaskDelay(6000 / portTICK_PERIOD_MS);
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para la lectura de la humedad del suelo
+*/
+void tareaHumedadSuelo(void *param)
+{
+  for (;;)
+  {
+    //Map["hs"] = 15.0;
+    //El sensor registra valores normales de humedad, no es necesaria conversión. Valor registrado = 33~34
+    Map["hs"] = SENSORSUELO; //Si se introduce con analogRead, obtenemos el valor en millares y decimales. Se puede utilizar si queremos se más precisos
+    xQueueSend(queue, (void *)&Map, (TickType_t)0);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+  vTaskDelete(NULL);
+}
+
+/*
+  Tarea para Enviar la información a la arquitectura
+*/
+void tareaEnvio(void *param)
+{
+  for (;;)
+  {
+    
+    xQueueReceive(queue, &Map, (TickType_t)(1000 / portTICK_PERIOD_MS));
+    String jsonData = "{\"temperatura\":" + String(Map["t"]) + ",\"fotoreceptor\":" + String(Map["f"]) + ",\"humedad\":" + String(Map["h"]) + ",\"humedadSuelo\":" + String(Map["hs"]) + "}";
+    Serial.printf("\nEnviando información...");
+    
+    wifiConnect();
+    mqttConnect();
+    
+    client.publish(TOPIC, jsonData.c_str());
+
+    nodeRed message = nodeRed_init_zero;
+    message.temperatura = Map["t"];
+    message.humedad = Map["h"];
+    message.humedadSuelo = Map["hs"];
+    message.fotoreceptor = Map["f"];
+    message.has_temperatura = true;
+    message.has_humedad = true;
+    message.has_humedadSuelo = true;
+    message.has_fotoreceptor = true;
+
+    if (Map["h"] < 30 || Map["hs"] < 30) {
+      servo();
+    }
+
+    uint8_t buffer[200];
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    bool status = pb_encode(&stream, nodeRed_fields, &message);
+    if (!status)
+      Serial.println("Error encode");
+    client.publish(TOPICNODERED, buffer, stream.bytes_written);
+    wifiDisconnect();
+    vTaskDelay(7000 / portTICK_PERIOD_MS);
+  }
+  vTaskDelete(NULL);
+}
+
+
+
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(LIGHTSENSORPIN, INPUT);
+  pinMode(SENSORSUELO, INPUT);
   pinMode(BTN_MOTOR, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(BTN_MOTOR), &motor_interrupcion, FALLING);
   Serial.begin(115200);
   dht.begin();
+
   wifiConnect();
   mqttConnect();
 
   //Inicialización del servo
-  /**mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_NUM_14);
+  mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, GPIO_NUM_14);
 
   mcpwm_config_t pwm_config;
     pwm_config.frequency = 50;    //frequency = 50Hz, i.e. for every servo motor time period should be 20ms
@@ -222,7 +261,16 @@ void setup()
     pwm_config.cmpr_b = 0;    //duty cycle of PWMxb = 0
     pwm_config.counter_mode = MCPWM_UP_COUNTER;
     pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
-  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);*/
+  mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+
+  esp_sleep_enable_timer_wakeup(10*1000000); //10 segundos
+  
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 10000000, true);
+  timerAlarmEnable(timer);
+
+  wifiDisconnect();
 
   queue = xQueueCreate(10, sizeof(Map));
   if (queue != NULL)
